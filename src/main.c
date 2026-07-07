08954bf1f4d7d34e792bc374b5824da6c6f53943
gumperto <162938203+Gumperto@users.noreply.github.com>
2026-07-08 00:00:47 +0900

    Add data structures + some test code in main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,27 @@
+// IMPORTANT: please keep main.c as small as possible;
+// everything here is just placeholder as a proof of 
+// concept at the time being (I do mean EVERYTHING)
+
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "structs.h"
+
+#define MAX_LENGTH 128
 
 int main() {
-    int a = 4, b = 7;
-    printf("Hello World!\n");
+    // testing includes
+    Course foundationsOfStatistics;
+    foundationsOfStatistics.title = (char*)malloc(MAX_LENGTH * sizeof(char));
+    strcpy(foundationsOfStatistics.title, "Foundations of Statistics");
+    foundationsOfStatistics.period = 5;
+    strcpy(foundationsOfStatistics.weekday, "Friday");
+
+    printf("Title: %s, Weekday: %s, Period: %d\n", foundationsOfStatistics.title,
+                                                   foundationsOfStatistics.weekday,
+                                                   foundationsOfStatistics.period);
+
+    free(foundationsOfStatistics.title);
+
     return 0;
 }
---
+++ src/structs.h
@@ -0,0 +1,20 @@
+#ifndef STRUCTS
+#define STRUCTS
+
+/* HELP: if you feel like there's any other data field
+* that would be helpful to add (i.e bool mandatory, char*
+* lecturer, bool required, etc) feel free to add it below */
+typedef struct {
+    char* title;
+    char weekday[16];
+    int period;
+}Course;
+
+/* HELP: what do we actually want the Schedule struct 
+* to do? like what benefit does it offer over just an
+* array of Course (i.e Course*)                    */
+typedef struct {
+    Course* courseList;
+}Schedule;
+
+#endif
