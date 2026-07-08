65e2ef8ae1d8be98908fd316c0f7af7db50b0449
Lucaseven <141701127+Lucaseven@users.noreply.github.com>
2026-07-08 22:56:36 +0900

    Changed structs and created addRequiredCourses function, logical next step would be function that reads values from a txt file and turns them into struct Courses

--- src/main.c
+++ src/main.c
@@ -11,15 +11,15 @@
 
 int main() {
     // testing includes
-    Course foundationsOfStatistics;
+    Course foundationsOfStatistics = {0};
     foundationsOfStatistics.title = (char*)malloc(MAX_LENGTH * sizeof(char));
     strcpy(foundationsOfStatistics.title, "Foundations of Statistics");
-    foundationsOfStatistics.period = 5;
-    strcpy(foundationsOfStatistics.weekday, "Friday");
-
-    printf("Title: %s, Weekday: %s, Period: %d\n", foundationsOfStatistics.title,
-                                                   foundationsOfStatistics.weekday,
-                                                   foundationsOfStatistics.period);
+    foundationsOfStatistics.q1_timeheld[4][4] = 1;
+    foundationsOfStatistics.q2_timeheld[4][4] = 1;
+    foundationsOfStatistics.credits = 2;
+    foundationsOfStatistics.isRequired = 1;
+    strcpy(foundationsOfStatistics.category, "Math");
+    printf("Title: %s\n", foundationsOfStatistics.title);
 
     free(foundationsOfStatistics.title);
 
--- src/structs.h
+++ src/structs.h
@@ -6,16 +6,41 @@
 * lecturer, bool required, etc) feel free to add it below */
 typedef struct {
     char* title;
-    char weekday[16];
-    int period;
+    //[1,1] for full semester, [1,0] for first half only, [0,1] for summer only
+    int runduration [2];
+    //the matrix is 0 when there's no class and 1 when there is class: 
+    //if there's class at only Mon Period 3 only q1_timeheld[0][2] and q2_timeheld[0][2] will be 1, rest of matrix will be 0
+    int q1_timeheld[6][6];
+    int q2_timeheld[6][6];
+    int credits;
+    //1 for required, 0 for not required
+    int isRequired;
+    char category[20];
 }Course;
 
 /* HELP: what do we actually want the Schedule struct 
 * to do? like what benefit does it offer over just an
 * array of Course (i.e Course*)                    */
 typedef struct {
-    Course * courseList[6][6];
-    
+    Course * q1_courseList[6][6];
+    Course * q2_courseList[6][6];
 }Schedule;
 
+
+void addRequired(Schedule sc, Course * class, int numCourses){
+    for(int i = 0; i < numCourses; i++){
+        if((class + i) -> isRequired == 1 ){
+            for(int j = 0; j < 6; j++){
+                for(int k = 0; k < 6; k++){
+                    if(class[i].q1_timeheld[j][k] == 1){
+                        sc.q1_courseList[j][k] = (class + i);
+                    }
+                    if(class[i].q2_timeheld[j][k] == 1){
+                        sc.q2_courseList[j][k] = (class + i);
+                    }
+                }
+            }
+        }
+    }
+}
 #endif
