57cadfe9c849cbba2911a0d680f73c30c625ab3d
Lucaseven <141701127+Lucaseven@users.noreply.github.com>
2026-07-08 23:04:11 +0900

    fixed just one typo

--- src/structs.h
+++ src/structs.h
@@ -28,7 +28,7 @@ typedef struct {
 
 
 //we should work on this thing next, all I did was open and close the file, should this be part of main?
-void readfile(char coursesTXT){
+void readfile(char * coursesTXT){
     FILE *fptr;
     fptr = fopen(coursesTXT,"r");
     if(fptr == NULL){
