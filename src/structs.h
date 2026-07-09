4a3ce9f4594e40426cf6389b0b8354ea48e11457
gumperto <162938203+Gumperto@users.noreply.github.com>
2026-07-10 02:12:55 +0900

    Modified structs for later use

--- src/structs.h
+++ src/structs.h
@@ -1,51 +1,76 @@
 #ifndef STRUCTS
 #define STRUCTS
 
-enum {
+#define ALLOCATION_LIMIT 128
+
+typedef enum {
+    TWO = 2
+}Tuple;
+
+typedef enum {
+    MONDAY = 0,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}Weekdays;
+
+typedef enum {
+    QUARTER_ONE = 0,
+    QUARTER_TWO = 1,
     SEMESTER_DURATION = 2,
     NUM_PERIODS = 6,
     NUM_WEEKDAYS = 6
-};
+}ScheduleConstants;
 
-/* HELP: if you feel like there's any other data field
-* that would be helpful to add (i.e bool mandatory, char*
-* lecturer, bool required, etc) feel free to add it below */
+typedef struct {
+    int tuple[TWO];
+}Duo;
+
+typedef struct {
+    Duo* list;
+    int size;
+}DuoList;
+
 typedef struct {
     char* title;
-    //[1,1] for full semester, [1,0] for first half only, [0,1] for summer only
-    int whichQuarter[SEMESTER_DURATION];
-    
-    //the matrix is 0 when there's no class and 1 when there is class: 
-    //if there's class at only Mon Period 3 for full semester, only q1_timeheld[0][2] = 1
-    //                                                         and  q2_timeheld[0][2] = 1
-    //                                                         rest of matrix         = 0
-    int q1_timeHeld[NUM_PERIODS][NUM_WEEKDAYS];
-    int q2_timeHeld[NUM_PERIODS][NUM_WEEKDAYS];
+
+    // array of Duo's, showing what weekday and period the class takes place in
+    DuoList meetings;
     
     int credits;
 
     //1 for required, 0 for not required
     int isRequired;
-    
+ 
+    //1 for have taken in the past, 0 for not taken in the past (unused)
+    int takenCredit;
+
     char* category;
 }Course;
 
-/* HELP: what do we actually want the Schedule struct 
-* to do? like what benefit does it offer over just an
-* array of Course (i.e Course*)                    */
 typedef struct {
-    Course* q1_courseList[NUM_PERIODS][NUM_WEEKDAYS];
-    Course* q2_courseList[NUM_PERIODS][NUM_WEEKDAYS];
+    // Stores the list of course pointers in memory
+    Course** courseList;
+    int courseCountTotal;
+
+    // Separate schedules for q1 and q2 so no collisions
+    Course* schedule[SEMESTER_DURATION][NUM_PERIODS][NUM_WEEKDAYS];
+    int courseCountTaken;
 
     // These two flags should probably be a placeholder
     // for now. Ideally we want the user to pick what periods
     // they don't want (1st period) and what days they don't 
     // want (Saturday, Monday, etc)
-    int saturday;
-    int period6;
+    int saturdayCheck;
+    int period6Check;
 
     int totalCredits;
     int targetCredits;
+
+    int allocationLimit;
 }Schedule;
 
 #endif
